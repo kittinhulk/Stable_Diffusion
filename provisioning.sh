a4cd63ec87bf599f31372659f2de445a1efab55e
Grit Ai Art <hulk19981@gmail.com>
2024-07-05 12:57:21 +0700

    Update provisioning.sh

--- provisioning.sh
+++ provisioning.sh
@@ -48,6 +48,7 @@ CHECKPOINT_MODELS=(
     "https://huggingface.co/GritTin/modelsStableDiffusion/resolve/main/pinkiepiePonyMix_v30BetaFp16.safetensors"
     "https://huggingface.co/GritTin/modelsStableDiffusion/resolve/main/waiREALMIX_v80.safetensors"
     "https://huggingface.co/GritTin/modelsStableDiffusion/resolve/main/waiREALCN_v60.safetensors"
+    "https://huggingface.co/GritTin/modelsStableDiffusion/resolve/main/tPonynai3_v55.safetensors"
     #"https://huggingface.co/GritTin/modelsStableDiffusion/resolve/main/leosamsHelloworldXL_helloworldXL70.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
