beeba878440b94c4805204a628f46b78fd0248d5
Grit Ai Art <hulk19981@gmail.com>
2024-08-06 10:54:35 +0700

    Update provisioning.sh

--- provisioning.sh
+++ provisioning.sh
@@ -44,6 +44,7 @@ NODES=(
     "https://github.com/Gourieff/comfyui-reactor-node"
     "https://github.com/jamesWalker55/comfyui-various"
     "https://github.com/cubiq/ComfyUI_InstantID"
+    "https://github.com/PowerHouseMan/ComfyUI-AdvancedLivePortrait"
     #"https://github.com/griptape-ai/ComfyUI-Griptape"
     #"https://github.com/zhongpei/Comfyui_image2prompt"
 )
