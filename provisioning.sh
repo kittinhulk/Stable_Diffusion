b4301351e0f1d4c38a70e9937a046aef2f467905
Grit Ai Art <hulk19981@gmail.com>
2024-09-01 12:07:27 +0700

    Update provisioning.sh

--- provisioning.sh
+++ provisioning.sh
@@ -45,6 +45,7 @@ NODES=(
     "https://github.com/sipherxyz/comfyui-art-venture"
     #"https://github.com/griptape-ai/ComfyUI-Griptape"
     #"https://github.com/zhongpei/Comfyui_image2prompt"
+    "https://github.com/comfyanonymous/ComfyUI_bitsandbytes_NF4"
 )
 
 CHECKPOINT_MODELS=(
