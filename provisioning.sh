abe81229c8d2771e284ffcbba2eb03d878b0405c
Grit Ai Art <hulk19981@gmail.com>
2024-10-20 00:37:54 +0700

    Update provisioning.sh

--- provisioning.sh
+++ provisioning.sh
@@ -197,8 +197,7 @@ UNET=(
     #"https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/flux1-schnell.sft"
     "https://huggingface.co/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-Q8_0.gguf"
     #"https://huggingface.co/GritTin/modelsStableDiffusion/resolve/main/colorfulasiangirlFlux_beta.safetensors"
-
-    #"https://huggingface.co/GritTin/modelsStableDiffusion/resolve/main/myhumanFluxTrainable_12Train.safetensors"
+    "https://huggingface.co/GritTin/modelsStableDiffusion/resolve/main/myhumanFluxTrainable_12Train.safetensors"
 )
 
 CLIP=(
